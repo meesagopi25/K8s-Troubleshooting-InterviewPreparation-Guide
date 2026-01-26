a854e912391204c9caa4423c824a603137c442e1
meesagopi25 <meesagopi25@gmail.com>
2026-01-26 14:44:21 +0530

    Update asg.tf

--- 06-UpgradeProcess/04-Upgrade-Data Plane/eks-selfmanaged-upgrade/terraform/asg.tf
+++ 06-UpgradeProcess/04-Upgrade-Data Plane/eks-selfmanaged-upgrade/terraform/asg.tf
@@ -1,1 +1,25 @@
 
+resource "aws_autoscaling_group" "workers_v2" {
+  name                = "eks-workers-v2"
+  min_size            = var.min_size
+  max_size            = var.max_size
+  desired_capacity    = var.desired_capacity
+  vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.workers_v2.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "kubernetes.io/cluster/${var.cluster_name}"
+    value               = "owned"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "nodegroup"
+    value               = "workers-v2"
+    propagate_at_launch = true
+  }
+}
