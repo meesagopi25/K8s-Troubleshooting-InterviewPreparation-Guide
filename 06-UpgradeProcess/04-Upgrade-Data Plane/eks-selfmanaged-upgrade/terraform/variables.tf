39fa5ffb161c8bfd44e4b7f90e1e62c024a69c1d
meesagopi25 <meesagopi25@gmail.com>
2026-01-26 14:58:46 +0530

    Update variables.tf

--- 06-UpgradeProcess/04-Upgrade-Data Plane/eks-selfmanaged-upgrade/terraform/variables.tf
+++ 06-UpgradeProcess/04-Upgrade-Data Plane/eks-selfmanaged-upgrade/terraform/variables.tf
@@ -1,1 +1,44 @@
 
+variable "aws_region" {
+  description = "AWS region where EKS cluster exists"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "worker_ami_id" {
+  description = "AMI ID for new self-managed worker nodes"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+  default     = "t3.large"
+}
+
+variable "subnet_ids" {
+  description = "Private subnet IDs for worker nodes"
+  type        = list(string)
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 5
+}
+
+variable "desired_capacity" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 3
+}
