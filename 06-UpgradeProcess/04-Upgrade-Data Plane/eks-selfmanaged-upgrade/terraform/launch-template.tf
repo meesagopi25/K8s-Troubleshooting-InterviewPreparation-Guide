0d54f5edfdc210906052782334833cdf550deb5d
meesagopi25 <meesagopi25@gmail.com>
2026-01-26 14:42:43 +0530

    Update launch-template.tf

--- 06-UpgradeProcess/04-Upgrade-Data Plane/eks-selfmanaged-upgrade/terraform/launch-template.tf
+++ 06-UpgradeProcess/04-Upgrade-Data Plane/eks-selfmanaged-upgrade/terraform/launch-template.tf
@@ -1,1 +1,26 @@
+resource "aws_launch_template" "workers_v2" {
+  name_prefix   = "eks-workers-v2-"
+  image_id      = var.worker_ami_id
+  instance_type = var.instance_type
+
+  user_data = base64encode(templatefile(
+    "${path.module}/bootstrap.sh",
+    {
+      cluster_name = var.cluster_name
+    }
+  ))
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name      = "eks-workers-v2"
+      nodegroup = "workers-v2"
+    }
+  }
+}
 
