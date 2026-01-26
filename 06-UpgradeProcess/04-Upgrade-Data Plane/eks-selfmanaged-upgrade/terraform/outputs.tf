ad74d36f24681515b0b85d10e1962981118a25c0
meesagopi25 <meesagopi25@gmail.com>
2026-01-26 15:00:27 +0530

    Update outputs.tf

--- 06-UpgradeProcess/04-Upgrade-Data Plane/eks-selfmanaged-upgrade/terraform/outputs.tf
+++ 06-UpgradeProcess/04-Upgrade-Data Plane/eks-selfmanaged-upgrade/terraform/outputs.tf
@@ -1,1 +1,20 @@
 
+output "new_asg_name" {
+  description = "Name of the newly created Auto Scaling Group"
+  value       = aws_autoscaling_group.workers_v2.name
+}
+
+output "new_launch_template_id" {
+  description = "Launch Template ID for new worker nodes"
+  value       = aws_launch_template.workers_v2.id
+}
+
+output "new_launch_template_version" {
+  description = "Latest launch template version"
+  value       = aws_launch_template.workers_v2.latest_version
+}
+
+output "worker_node_labels" {
+  description = "Node label applied to new worker nodes"
+  value       = "nodegroup=workers-v2"
+}
